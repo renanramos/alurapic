d565a2f4791955bcd012e0edcc935c0dfb47996d
Renan Ramos <=>
2019-09-25 16:52:05 -0300

    Inclusão de componente PhotoComponente

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Photo } from '../../photo/photo';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnChanges {
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+  constructor() { }
+
+  ngOnInit() {        
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    } 
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index += 3){
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
+  }
+
+}
