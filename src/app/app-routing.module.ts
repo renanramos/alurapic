268f87af4fa3f68ad93887de036d367982d6ae51
Renan Ramos <=>
2019-09-25 16:02:30 -0300

    Form Photos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotoFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
