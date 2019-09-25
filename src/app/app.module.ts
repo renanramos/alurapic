69efc894bab4125eefc07aeead1f3d51c1eb61ef
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-09-24 22:41:28 -0300

    Criação de novo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PhotoComponent } from './photo/photo.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
