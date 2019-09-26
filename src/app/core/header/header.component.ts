ec1ad207ce22c6dc04d6edddc6d69b7d7378a893
Renan Ramos <renanrramossi@gmail.com>
2019-09-26 15:31:33 -0300

    Continuação da construção do component header. Decode de token com jwt. Reorganização do código.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs';
+
+import { User } from '../user/user';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+
+    user$: Observable<User>;
+
+    constructor(userService: UserService) {
+        this.user$ = userService.getUser();
+    }
+ }
