574ecacafdcae3d1f8e1df226842e4ed27090226
Renan Ramos <renanrramossi@gmail.com>
2019-09-26 14:08:02 -0300

    Implementação de método de autenticação

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
+import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/component/vmessage/vmessage.module';
 
 
@@ -11,7 +12,8 @@ import { VMessageModule } from '../shared/component/vmessage/vmessage.module';
     imports: [ 
         CommonModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
      ]
 })
 export class HomeModule {
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './signin.component.html'
 })
 export class SignInComponent implements OnInit { 
+    
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router) { }
 
-    ngOnInit() {
+    ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
     }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService.authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                }
+            )
+    }
+
 }
