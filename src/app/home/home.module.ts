04d72902d789459c9e691748730116406685fa4f
Renan Ramos <renanrramossi@gmail.com>
2019-09-26 16:06:04 -0300

    Implementação da página SignUp

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/component/vmessage/vmessage.module';
+import { SignUpComponent } from './signup/signup.component';
 
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent 
+    ],
     imports: [ 
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
---
+++ src/app/home/signup/signup.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    templateUrl: './signup.component.html'
+})
+export class SignUpComponent implements OnInit {
+
+    signupForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.signupForm = this.formBuilder.group({
+            email: ['',
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['', 
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName: ['', 
+                [
+                    Validators.required,
+                    Validators.pattern(/^[a-z0-9_\-]+$/),
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ]
+            ],
+            password: ['', 
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(14)
+                ]
+            ]
+        })
+    }
+
+}
