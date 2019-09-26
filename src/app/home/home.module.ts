c3f62844d46064963947214766518fc79906eba7
Renan Ramos <renanrramossi@gmail.com>
2019-09-26 13:46:57 -0300

    Implementação e configuração do componente de mensagem

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/component/vmessage/vmessage.module';
 
 
 @NgModule({
     declarations: [ SignInComponent ],
     imports: [ 
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        VMessageModule
      ]
 })
 export class HomeModule {
---
+++ src/app/shared/component/vmessage/vmessage.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-vmessage',
+    templateUrl: './vmessage.component.html'
+})
+export class VMessageComponent {
+
+    @Input() text: string = '';
+
+}
