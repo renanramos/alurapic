6ef580d6e755085fbc82da2e0d69d3023912b05c
Renan Ramos <renanrramossi@gmail.com>
2019-09-26 13:39:08 -0300

    Implementação da página de login

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent { }
+export class SignInComponent implements OnInit { 
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+}
