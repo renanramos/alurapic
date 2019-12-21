6991b2e0e390c79cbf5759a80975e4ae4f1f8867
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-12-21 01:32:36 -0300

    Alteração de variáveis de ambiente 'environment'

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -5,8 +5,9 @@ import { Photo } from './photo';
 import { PhotoComment } from './photo-comment';
 import { map, catchError } from 'rxjs/operators';
 import { of, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
-const API = 'http://localhost:3000/';
+const API = environment.ApiUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,7 @@ export class PhotoService {
 
   listFormUser(userName: string) {
     return this.http
-      .get<Photo[]>(API + `${userName}/photos`)
+      .get<Photo[]>(`${API}/${userName}/photos`)
   }
 
   listFromUserPaginated(userName: string, page: number) {
@@ -25,7 +26,7 @@ export class PhotoService {
       .append('page', page.toString());
 
     return this.http
-      .get<Photo[]>(API + `${userName}/photos`, { params });
+      .get<Photo[]>(`${API}/${userName}/photos`, { params });
   }
 
   upload(description: string, allowComments: boolean, file: File) {
@@ -35,27 +36,27 @@ export class PhotoService {
     formData.append('allowComments', allowComments ? 'true' : 'false');
     formData.append('imageFile', file);
 
-    return this.http.post(API + 'photos/upload', formData);
+    return this.http.post(`${API}/photos/upload`, formData);
   }
 
   findById(photoId: number) {
-    return this.http.get<Photo>(`${API}photos/${photoId}`);
+    return this.http.get<Photo>(`${API}/photos/${photoId}`);
   }
 
   getComments(photoId: number) {
-    return this.http.get<PhotoComment[]>(`${API}photos/${photoId}/comments`);
+    return this.http.get<PhotoComment[]>(`${API}/photos/${photoId}/comments`);
   }
 
   addComment(photoId: number, commentText: string) {
-    return this.http.post(`${API}photos/${photoId}/comments`, { commentText })
+    return this.http.post(`${API}/photos/${photoId}/comments`, { commentText })
   }
 
   removePhoto(photoId: number) {
-    return this.http.delete(`${API}photos/${photoId}`);
+    return this.http.delete(`${API}/photos/${photoId}`);
   }
 
   like(photoId: number) {
-    return this.http.post(`${API}photos/${photoId}/like`, {}, { observe: 'response' })
+    return this.http.post(`${API}/photos/${photoId}/like`, {}, { observe: 'response' })
       .pipe(map(res => true))
       .pipe(catchError(err => {
         return err.status == '304' ? of(false) : throwError(err);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  ApiUrl: 'http://sua-api-aqui:3000'
 };
