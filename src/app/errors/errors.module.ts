69ed7a9e74d82a89f7f3f619e147e0c6f5867ea4
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2020-01-11 00:09:19 -0300

    Implementação de log de erro customizado

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
-
-
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   declarations: [ NotFoundComponent ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
   ]
 })
 export class ErrorsModule { }
---
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -0,0 +1,11 @@
+import { ErrorHandler } from '@angular/core';
+
+export class GlobalErrorHandler implements ErrorHandler {
+
+  handleError(error: any) {
+    console.log(error);
+    throw error;
+    
+  }
+
+}
