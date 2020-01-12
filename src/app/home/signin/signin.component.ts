f01e5b9b327d75d3f53955dbfdc8fbb554484f1b
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2020-01-11 22:33:25 -0300

    Query params

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth/auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
@@ -9,6 +9,7 @@ import { PlatformDetectorService } from 'src/app/core/platform-detector/platform
 })
 export class SignInComponent implements OnInit { 
     
+    fromUrl: string;
     loginForm: FormGroup;
     @ViewChild('userNameInput', { static: false }) userNameInput: ElementRef<HTMLInputElement>;
 
@@ -16,9 +17,14 @@ export class SignInComponent implements OnInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService) { }
+        private platformDetectorService: PlatformDetectorService,
+        private activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
+        this.activatedRoute
+            .queryParams
+            .subscribe(params => this.fromUrl = params['fromUrl']);
+
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
@@ -30,7 +36,11 @@ export class SignInComponent implements OnInit {
         const password = this.loginForm.get('password').value;
         this.authService.authenticate(userName, password)
             .subscribe(
-                () => this.router.navigate(['user', userName]),
+                () => {
+                    this.fromUrl 
+                        ? this.router.navigateByUrl(this.fromUrl) 
+                        : this.router.navigate(['user', userName]);
+                },
                 err => {
                     this.loginForm.reset();
                     this.platformDetectorService.isPlatformBrowser() &&
