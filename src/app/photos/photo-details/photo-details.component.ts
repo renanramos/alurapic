ae38d45c30e70c36e9403c8cdd4b26b4dcdf01dc
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2020-01-11 22:22:46 -0300

    Impedindo voltar a página

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -35,7 +35,7 @@ export class PhotoDetailsComponent implements OnInit {
       .removePhoto(this.photoId)
       .subscribe(() => {
         this.alertService.success('Photo removed', true);
-        this.router.navigate(['/user', this.userService.getUserName()]);
+        this.router.navigate(['/user', this.userService.getUserName()], { replaceUrl: true });
       },
       (err) => {
         this.alertService.warning('Could not delete the photo', true);
